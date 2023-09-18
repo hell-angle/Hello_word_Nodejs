4a8f92903f1b67baf69ae5ab2b8c47e19a0d7372
ĐỖ TRÌNH <20522058@gm.uit.edu.vn.com>
2023-09-18 16:27:45 +0700

    update code

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
-
+ 
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
